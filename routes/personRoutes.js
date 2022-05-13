3b1bf91246ce6b51a7780dd3b5a6b6b6bd13a1b0
crisholiveira <cris.ho.soares@gmail.com>
2022-05-13 10:38:38 -0300

    rota get

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -8,6 +8,7 @@ router.post('/', async (req, res)=> {
 
     if(!name) {
         res.status(422).json({error: "o nome é obrigatório"})
+        return
     }
 
     const person = {
@@ -19,6 +20,33 @@ router.post('/', async (req, res)=> {
     try {
         await Person.create(person)
         res.status(201).json({message: "Pessoa criada com sucesso"})
+    } catch (error) {
+        res.status(500).json({error: error})
+        
+    }
+})
+
+router.get('/', async (req, res)=>{
+    try {
+        const people = await Person.find()
+        res.status(200).json(people)
+    } catch (error) {
+        res.status(500).json({error: error})
+        
+    }
+} )
+
+router.get('/:id', async (req, res)=> {
+    const id = req.params.id 
+  
+    try {
+        const person = await Person.findOne({_id:id})
+        if(!person){
+            res.status(422).json({message: "o usuario não foi encontrado"})
+            return
+        }
+        res.status(200).json(person)
+        
     } catch (error) {
         res.status(500).json({error: error})
         
